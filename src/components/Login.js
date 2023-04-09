d16543c9c94d1ad26b4b2675232503f615202fc6
Alejandro.pc <alejandropatinoc@gmail.com>
2023-04-08 18:46:56 -0700

    users can successfully login - need to add user authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,11 +17,46 @@ function Login() {
 
   // Will take the user to profile page if they are signing up or game lobby if they signed in.
 
-  async function submitForm(e) {
+  /** user registartion  */
+  async function registrationForm(e) {
     e.preventDefault();
     console.log(e);
 
+    /* connecting to backend routes */
     await fetch("http://localhost:8000/api/v1/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        email: email,
+        password: password
+      }),
+    })
+      .catch(error => {
+        window.alert(error);
+        return;
+      });
+
+    const inputValue = e.target.username.value;
+    if (showSignUpForm === true) {
+      setUserName(inputValue);
+      navigate("/ProfilePage");
+    } else {
+      setUserName(inputValue);
+      navigate("/GameLobby");
+    }
+  }
+
+  //TODO: add error handling for incorrect username/password
+  /** user log in */
+  async function loginForm(e) {
+    e.preventDefault();
+    console.log(e);
+
+    /* connecting to backend routes */
+    await fetch("http://localhost:8000/api/v1/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -54,7 +89,7 @@ function Login() {
       {showSignUpForm ? (
         <>
           <h1>Sign Up</h1>
-          <form onSubmit={(e) => submitForm(e)}>
+          <form onSubmit={(e) => registrationForm(e)}>
             <label>username: </label>
             <input name="username" onChange={(event) => {
               setUserName(event.target.value);
@@ -76,11 +111,15 @@ function Login() {
       ) : (
         <>
           <h1>Login</h1>
-          <form onSubmit={(e) => submitForm(e)}>
+          <form onSubmit={(e) => loginForm(e)}>
             <label>username: </label>
-            <input name="username"></input>
+            <input name="username" onChange={(event) => {
+              setUserName(event.target.value);
+            }}></input>
             <label>password: </label>
-            <input name="password"></input>
+            <input name="password" onChange={(event) => {
+              setPassword(event.target.value);
+            }}></input>
             <button type="submit">Login!</button>
           </form>
           <button onClick={toggleForm}>Dont have an account? Register </button>
